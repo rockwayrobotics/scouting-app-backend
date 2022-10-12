f508e5a76b72bad05d7b2ae484311cc7c134aeda
Erin Nova <contact@the-system.eu.org>
2022-10-12 15:33:48 -0400

    Setup database

--- frontend/GUI/models.py
+++ frontend/GUI/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+class TeamData(models.Model):
+    id = models.IntegerField(primary_key=True) # team ID
+    name = models.CharField(max_length=200) # name of the team
+    record_date = models.DateField() # when the data was recorded
+
+    def __str__(self):
+        return str(self.id)
