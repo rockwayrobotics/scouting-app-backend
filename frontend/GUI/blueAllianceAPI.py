68333003da1f3ef02c6a9e96ad56b6f328b44e97
ovandermeer <owenvandermeer@gmail.com>
2022-10-25 09:21:29 -0400

    Allowed loading API key from .env file via pipenv

--- frontend/GUI/blueAllianceAPI.py
+++ frontend/GUI/blueAllianceAPI.py
@@ -1,12 +1,13 @@
 from email import header
 import requests
 import json
+import os
 
 from django.core.exceptions import ObjectDoesNotExist
 
 from .models import TeamData, matchResult, event, team, registration
 
-my_headers={"X-TBA-Auth-Key":"TBA API key goes here"}
+my_headers={"X-TBA-Auth-Key":os.getenv('TBA_AUTH')}
 
 def getData():
     result = json.loads(requests.get("https://www.thebluealliance.com/api/v3/team/frc8089/events/simple", headers=my_headers).text)
