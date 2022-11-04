92227b5aed76e9bd59875d333c846069bdd79e3c
ovandermeer <owenvandermeer@gmail.com>
2022-11-04 13:23:33 -0400

    Update to load SECRET_KEY in settings.py from environment variable, to avoid committing the key to GitHub

--- GUI/blueAllianceAPI.py
+++ GUI/blueAllianceAPI.py
@@ -1,4 +1,3 @@
-from email import header
 import requests
 import json
 import os
@@ -7,10 +6,12 @@ from django.core.exceptions import ObjectDoesNotExist
 
 from .models import MatchResult, Event, Team, Registration
 
-my_headers = {"X-TBA-Auth-Key":os.getenv('TBA_AUTH')}
+my_headers = {"X-TBA-Auth-Key": os.getenv('TBA_AUTH')}
+
 
 def get_data():
-    result = json.loads(requests.get("https://www.thebluealliance.com/api/v3/team/frc8089/events/simple", headers=my_headers).text)
+    result = json.loads(
+        requests.get("https://www.thebluealliance.com/api/v3/team/frc8089/events/simple", headers=my_headers).text)
 
     for i in result:
         if i['year'] == 2022:
@@ -18,7 +19,8 @@ def get_data():
             try:
                 Event.objects.get(event_key=i['key'])
             except ObjectDoesNotExist:
-                result = json.loads(requests.get(f"https://www.thebluealliance.com/api/v3/event/{i['key']}/teams", headers=my_headers).text)
+                result = json.loads(
+                    requests.get(f"https://www.thebluealliance.com/api/v3/event/{i['key']}/teams", headers=my_headers).text)
 
                 for item in result:
                     print("Team " + str(item['team_number']) + " - " + item['nickname'])
