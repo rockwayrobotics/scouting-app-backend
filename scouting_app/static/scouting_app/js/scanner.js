c89700bba3a3494ef69487c9443148da67c35477
Erin Abicht <contact@the-system.eu.org>
2023-03-13 12:12:52 -0400

    Can parse QR code from new scouting app (uncompressed)

--- scouting_app/static/scouting_app/js/scanner.js
+++ scouting_app/static/scouting_app/js/scanner.js
@@ -1,39 +1,28 @@
 let soundplaying = false;
-async function onScanSuccess(decodedText, decodedResult) {
-    playAudio();
 
-    await new Promise(r => setTimeout(r, 1000));
-
-    let my_dict2 = JSON.parse(decodedText);
-    // Get data from QR code
-    // Dictionary data must match the names of the fields in the HTML form
-    for(const key in my_dict2) {
-        document.getElementById(key).value = my_dict2[key];
-    }
-
-    const auto_submit_box = document.getElementById('auto_submit')
-
-    if(auto_submit_box.checked) {
-        document.getElementById('myForm').submit();
-    }
+function convertBinaryToObject(str) {
+	var newBin = str.split(" ");
+	var binCode = [];
+	for (i = 0; i < newBin.length; i++) {
+		binCode.push(String.fromCharCode(parseInt(newBin[i], 2)));
+	}
+	let jsonString = binCode.join("");
+	return JSON.parse(jsonString);
 }
 
-function clearForm() {
-    let formElements = document.getElementsByTagName('input');
-    for(i=0;i<formElements.length;i++) {
-        if(formElements[i].type === 'text') {
-            formElements[i].value='';
-        }
-    }
+function onScanSuccess(decodedText, decodedResult) {
+	console.log(`Code matched = ${decodedText}`, decodedResult);
+	console.log(convertBinaryToObject(decodedText));
 }
 
-function playAudio() {
-    var audio = new Audio("/static/elegant-notification-sound.mp3");
-    if(!soundplaying) {
-        audio.play();
-        soundplaying = true;
-    }
+function onScanFailure(error) {
+  // handle scan failure, usually better to ignore and keep scanning.
+  // for example:
+  console.warn(`Code scan error = ${error}`);
 }
 
-let html5QrcodeScanner = new Html5QrcodeScanner("reader", { fps: 10, qrbox: 300 });
-    html5QrcodeScanner.render(onScanSuccess);
+let html5QrcodeScanner = new Html5QrcodeScanner(
+  "reader",
+  { fps: 10, qrbox: {width: 250, height: 250} },
+  /* verbose= */ false);
+html5QrcodeScanner.render(onScanSuccess, onScanFailure);
